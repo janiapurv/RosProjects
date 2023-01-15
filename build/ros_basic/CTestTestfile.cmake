a0c1670c20bdf31d2e2fd828bf16c1cbc8275bbb
ajani <janiapurv95@gnail.com>
2023-01-15 15:31:34 -0800

    Added initial ros files

---
+++ build/ros_basic/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ajani/Apurv/src/ros_basic
+# Build directory: /home/ajani/Apurv/build/ros_basic
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
